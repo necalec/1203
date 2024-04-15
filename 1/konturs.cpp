94d8e505a4c8517c55e03b11326e71d837f71a70
Nikita Zhukov <necalec@gmail.com>
2024-04-16 00:50:16 +0300

    main

---
+++ 1/konturs.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+RNG rng(12345);
+
+int one = 5;
+int two = 20;
+
+void onTrackbar(int, void*) {}
+
+string getShapeName(int sides) {
+    if (sides == 3)
+        return "triangle";
+    else if (sides == 4)
+        return "rectangle";
+    else
+        return "circle";
+}
+
+int main()
+{
+    string way = "notme.png";
+
+    Mat img_show = imread(way, 1);
+
+    if (img_show.empty()) {
+        cout << "nothing!" << endl;
+        return 0;
+    }
+    else {
+        cout << "norm" << endl;
+    }
+
+    resize(img_show, img_show, Size(img_show.cols, img_show.rows));
+
+    Mat gray;
+    Mat new_img;
+    Mat gauss;
+    Mat drawing;
+
+    cvtColor(img_show, gray, COLOR_BGR2GRAY, 0);
+    GaussianBlur(gray, gauss, Size(7, 7), 0);
+
+    namedWindow("drawing");
+    createTrackbar("1", "drawing", &one, 100, onTrackbar);
+    createTrackbar("2", "drawing", &two, 100, onTrackbar);
+
+    while (true) {
+        Canny(gauss, new_img, one, two, 3);
+
+        vector<vector<Point> > contours;
+        vector<Vec4i> hierarchy;
+        findContours(new_img, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+
+        cvtColor(new_img, drawing, COLOR_GRAY2BGR);
+
+        for (size_t i = 0; i < contours.size(); i++)
+        {
+            vector<Point> approx;
+            approxPolyDP(contours[i], approx, arcLength(contours[i], true) * 0.02, true);
+
+            Moments M = moments(approx);
+            Point2f center(M.m10 / M.m00, M.m01 / M.m00);
+
+            string shapeName = getShapeName(approx.size());
+
+            putText(drawing, shapeName, center, FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 255, 255), 2);
+        }
+
+        imshow("drawing", drawing);
+
+        int key = waitKey(30);
+    }
+
+    return 0;
+}
+
---
+++ 2/text.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+    string way = "image.jpg";
+
+    Mat img = imread(way, IMREAD_COLOR);
+
+    if (img.empty())
+    {
+        cerr << "no!" << endl;
+        return -1;
+    }
+
+    Mat gray;
+    cvtColor(img, gray, COLOR_BGR2GRAY);
+
+    Mat binary;
+    threshold(gray, binary, 60, 255, THRESH_BINARY);
+
+    Mat inverted_binary;
+    bitwise_not(binary, inverted_binary);
+
+    Mat white_bg(img.size(), img.type(), Scalar(255, 255, 255));
+
+    Mat gray_objects;
+    addWeighted(img, 0.5, Scalar(200, 200, 200), 0.5, 0, gray_objects);
+
+    gray_objects.copyTo(white_bg, inverted_binary);
+
+    namedWindow("Original Image", WINDOW_AUTOSIZE);
+    imshow("Original Image", img);
+
+    namedWindow("Result Image", WINDOW_AUTOSIZE);
+    imshow("Result Image", white_bg);
+
+    waitKey(0);
+
+    return 0;
+}
+//серый цвет чтобы делать расскраски
